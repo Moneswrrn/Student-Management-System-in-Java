da1356c689dbc6aa93c7454d3dc97384b37ba974
Sharvin1712 <sharvinsr1712@1utar.my>
2025-04-26 23:31:42 +0800

    initial commit for Sharvin branch

--- src/Table.java
+++ src/Table.java
@@ -1,29 +1,46 @@
+// Edited by Sharvin A/L S. Ramesg (Assignment 2)
 import java.sql.*;
 import java.util.Vector;
 
 import javax.swing.table.DefaultTableModel;
 
 public class Table {
-    DefaultTableModel buildTableModel(ResultSet rs) throws SQLException {
-        ResultSetMetaData metaData = rs.getMetaData();
+
+     /**
+     * Builds a table model from the given ResultSet.
+     * 
+     * @param resultSet The ResultSet containing data from a database.
+     * @return A DefaultTableModel containing the data and column names.
+     * @throws SQLException If any database access error occurs.
+     */
+
+    DefaultTableModel buildTableModel(ResultSet resultSet) throws SQLException {
+        ResultSetMetaData metaData = resultSet.getMetaData(); // get metadata from the ResultSet like information about columns
 
         // Get column names
         int columnCount = metaData.getColumnCount();
+
+        // create a list to store the column names
         Vector<String> columnNames = new Vector<String>();
+
+        // add each of the column names to the list
         for (int i = 1; i <= columnCount; i++) {
             columnNames.add(metaData.getColumnName(i));
         }
 
-        // Get row data
-        Vector<Vector<Object>> rowData = new Vector<Vector<Object>>();
-        while (rs.next()) {
+        // Get table data
+        Vector<Vector<Object>> tableData = new Vector<Vector<Object>>();
+
+        while (resultSet.next()) {
             Vector<Object> row = new Vector<Object>();
+
+            // for each column in the row add the value to the row vector
             for (int i = 1; i <= columnCount; i++) {
-                row.add(rs.getObject(i));
+                row.add(resultSet.getObject(i));
             }
-            rowData.add(row);
+            tableData.add(row); // add the row to the table data
         }
 
-        return new DefaultTableModel(rowData, columnNames);
+        return new DefaultTableModel(tableData, columnNames); // create and return a DefaultTableModel using data collected
     }
 }
