b3134f99efc7b8cac320d41ed96f30fc715b6af5
jr8868 <junrongtoh@1utar.my>
2025-04-27 01:58:49 +0800

    Added header comment and added comment line in each line of code

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,15 @@
 // Edited by JunRong test Assignment 2 
-import javax.swing.*;
+import javax.swing.*;// Import the Swing package for creating GUI components
 
 public class Main {
     public static void main(String[] args) {
+        // Create an instance of the AppGUI class (this will set up the GUI)
         AppGUI window = new AppGUI();
+        // Adjust the window size based on its content (preferred size of components)
         window.pack();
+     	// Make the window visible to the user
         window.setVisible(true);
+    	 // Set the application to exit when the window is closed
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 }
