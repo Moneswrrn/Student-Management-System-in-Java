803541396329f3e230550cc4514d409931af1aba
jr8868 <junrongtoh@1utar.my>
2025-04-27 02:10:58 +0800

    Update Main.java with new changes for report

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,15 @@
 // Edited by JunRong test Assignment 2 
-import javax.swing.*;// Import the Swing package for creating GUI components
+import javax.swing.*; // Import Swing library for GUI components
 
 public class Main {
     public static void main(String[] args) {
-        // Create an instance of the AppGUI class (this will set up the GUI)
+        // Create a new window from the AppGUI class
         AppGUI window = new AppGUI();
-        // Adjust the window size based on its content (preferred size of components)
+        // Adjust window size to fit components
         window.pack();
-     	// Make the window visible to the user
+        // Display the window on the screen
         window.setVisible(true);
-    	 // Set the application to exit when the window is closed
+    	// Close program when window is closed
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 }
